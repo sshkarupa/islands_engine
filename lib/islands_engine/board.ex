5394523c9c99d5d3c0ea4e6ad29c909b576fe337
Sergey Shkarupa <s.shkarupa@gmail.com>
2019-10-04 23:52:52 +0200

    Check island position

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -2,4 +2,19 @@ defmodule IslandsEngine.Board do
   alias IslandsEngine.{Board, Island}
 
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlaps_existing_islands?(board, key, island) do
+      true -> {:error, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
+
+  defp overlaps_existing_islands?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+      new_key != key and Island.overlap?(island, new_island)
+    end)
+  end
+
+  def all_islands_positioned?(board), do: Enum.all?(Island.types, &(Map.has_key?(board, &1)))
 end
