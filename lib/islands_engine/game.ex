a937f8e2925bcd076caa0efd2ac004aab59449fe
Sergey Shkarupa <s.shkarupa@gmail.com>
2019-10-06 20:16:52 +0200

    Add second player name

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -9,4 +9,24 @@ defmodule IslandsEngine.Game do
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
     {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
   end
+
+  def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
+
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player)
+    do
+      state
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state}
+    end
+  end
+
+  defp update_player2_name(state, name), do: put_in(state.player2.name, name)
+
+  defp update_rules(state, rules), do: %{state | rules: rules}
+
+  defp reply_success(state, reply), do: {:reply, reply, state}
 end
